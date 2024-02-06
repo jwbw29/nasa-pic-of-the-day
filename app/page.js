6dae7685933aa28f6f45d0bdf520e478f9ef8cbe
Justin Byrd <justinbyrd7@gmail.com>
2024-02-06 16:29:25 -0600

    Update dependencies and styles

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Stuff here
+      <div className="max-w-5xl w-full items-end justify-start font-mono lg:flex gap-4">
+        <h1 className="text-8xl tracking-wide ">APOD</h1>
+        <p className=" text-lg pb-2">Astronomy Picture of the Day</p>
+      </div>
+      <div className="max-w-5xl w-full">stuff</div>
     </main>
   );
 }
