fe122a856fa0b1397895761c490c95f4193a6c53
Justin Byrd <justinbyrd7@gmail.com>
2024-02-06 17:52:33 -0600

    trying to get real data to work

--- app/page.js
+++ app/page.js
@@ -1,12 +1,4 @@
 "use client";
-/*Steps for turning off real data and turning on fake data so we don't expire the key
-
-1. Comment out the fetchPhotoData function
-2. Comment out the useEffect hook
-3. Comment out the fetchPhotoData call in the useEffect hook
-4. uncomment the fakeData object
-5. uncomment everything with fakeData in it
-*/
 
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
@@ -14,20 +6,20 @@ import Image from "next/image";
 const API_KEY = "DEMO_KEY";
 const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
 
-const fakeData = {
-  copyright: "\nJordi L. Coy\n",
-  date: "2023-08-08",
-  explanation:
-    "What's that below the Moon? Jupiter -- and its largest moons.  Many skygazers across planet Earth enjoyed the close conjunction of Earth's Moon passing nearly in front of Jupiter in mid-June. The featured image is a single exposure of the event taken from Mor\u00f3n de la Frontera, Spain. The sunlit lunar crescent on the left is overexposed, while the Moon's night side, on the right, is only faintly illuminated by Earthshine. Lined up diagonally below the Moon, left to right, are Jupiter's bright Galilean satellites: Callisto, Ganymede, Io (hard to see as it is very near to Jupiter), and Europa.  In fact, Callisto, Ganymede, and Io are larger than Earth's Moon, while Europa is only slightly smaller. NASA's robotic spacecraft Juno is currently orbiting Jupiter and made a close pass near Io only a week ago.  If you look up in the night sky tonight, you will again see two of the brightest objects angularly close together -- because tonight is another Moon-Jupiter conjunction.",
-  hdurl: "https://apod.nasa.gov/apod/image/2308/MoonsJupiter_Coy_2630.jpg",
-  media_type: "image",
-  service_version: "v1",
-  title: "Moon Meets Jupiter",
-  url: "https://apod.nasa.gov/apod/image/2308/MoonsJupiter_Coy_960.jpg",
-};
-
 export default function Home() {
   const [photoData, setPhotoData] = useState(null);
+
+  const fakeData = {
+    copyright: "\nJordi L. Coy\n",
+    date: "2023-08-08",
+    explanation:
+      "What's that below the Moon? Jupiter -- and its largest moons.  Many skygazers across planet Earth enjoyed the close conjunction of Earth's Moon passing nearly in front of Jupiter in mid-June. The featured image is a single exposure of the event taken from Mor\u00f3n de la Frontera, Spain. The sunlit lunar crescent on the left is overexposed, while the Moon's night side, on the right, is only faintly illuminated by Earthshine. Lined up diagonally below the Moon, left to right, are Jupiter's bright Galilean satellites: Callisto, Ganymede, Io (hard to see as it is very near to Jupiter), and Europa.  In fact, Callisto, Ganymede, and Io are larger than Earth's Moon, while Europa is only slightly smaller. NASA's robotic spacecraft Juno is currently orbiting Jupiter and made a close pass near Io only a week ago.  If you look up in the night sky tonight, you will again see two of the brightest objects angularly close together -- because tonight is another Moon-Jupiter conjunction.",
+    hdurl: "https://apod.nasa.gov/apod/image/2308/MoonsJupiter_Coy_2630.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Moon Meets Jupiter",
+    url: "https://apod.nasa.gov/apod/image/2308/MoonsJupiter_Coy_960.jpg",
+  };
 
   // const fetchPhotoData = async () => {
   //   try {
@@ -44,16 +36,12 @@ export default function Home() {
     setPhotoData(fakeData);
   }, []);
 
-  const {
-    title,
-    date,
-    explanation,
-    url,
-    hdurl,
-    media_type,
-    copyright,
-    service_version,
-  } = fakeData;
+  // if (!photoData) {
+  //   //TODO replace this w/ a Loading component skeleton at some point
+  //   return <div>Loading...</div>;
+  // }
+
+  const { url, title, explanation, date, copyright } = photoData || fakeData;
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24 gap-24">
