fb8e71e3b38a4d8a1f3a75ef16054bb33a720677
Justin Byrd <justinbyrd7@gmail.com>
2024-02-06 16:56:17 -0600

    Refactor page.js layout and content

--- app/page.js
+++ app/page.js
@@ -13,13 +13,21 @@ export default function Home() {
           <Image />
           pic
         </div>
-        <div className="flex flex-col w-1/2 ">
+        <div className="flex flex-col w-1/2">
           <h1 className="text-4xl font-bold tracking-wider">Title</h1>
-          <div className="">
-            <h3 className="">copyright</h3>
-            <h3 className="">date</h3>
+          <div className="flex gap-8 pt-1">
+            <h3 className=" font-extrabold text-lg">copyright</h3>
+            <h3 className=" font-light">date</h3>
           </div>
-          <p className="">explanation</p>
+          <p className="pt-8">
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
         </div>
       </div>
     </main>
