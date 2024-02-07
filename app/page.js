c9aef57b6a8fc8548a1f70b41dda40928f86e416
Justin Byrd <justinbyrd7@gmail.com>
2024-02-06 21:21:25 -0600

    updated font

--- app/page.js
+++ app/page.js
@@ -42,8 +42,8 @@ export default async function Home() {
   const { url, title, explanation, date, copyright } = data;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24 gap-24">
-      <div className=" max-w-7xl w-full items-end justify-start font-mono lg:flex gap-4">
+    <main className="flex min-h-screen flex-col font-mono items-center justify-start p-24 gap-24">
+      <div className=" max-w-7xl w-full items-end justify-start lg:flex gap-4">
         <h1 className="text-8xl tracking-wide ">APOD</h1>
         <p className=" text-lg pb-2">Astronomy Picture of the Day</p>
       </div>
